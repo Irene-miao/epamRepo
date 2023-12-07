0d61a034ee2dc6b202d75f8c1c260b40b04e926d
Irene-miao <71961907+Irene-miao@users.noreply.github.com>
2023-12-07 15:38:34 +0800

    Add changes

--- src/main/java/com/example/demo/controller/TutorialController.java
+++ src/main/java/com/example/demo/controller/TutorialController.java
@@ -6,7 +6,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +23,69 @@ import com.example.demo.model.Tutorial;
 import com.example.demo.repository.TutorialRepository;
 
 @RestController
+@RequestMapping
 public class TutorialController {
 	  
+    @GetMapping(path="/tutorials")
+    public ResponseEntity<String> getTutorial() {
+         try { 
+
+         }catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(Json.createObjectBuilder()
+            .add("error", e.getMessage())
+            .build().toString());
+         }
+
+         return ResponseEntity.status(HttpStatus.OK)
+         .contentType(MediaType.APPLICATION_JSON)
+         .body(emotion);
+    }
+
+    @PostMapping(path="/tutorials/create")
+    public ResponseEntity<String> createTutorial() {
+         try {
+
+         }catch (Exception e) {
+             e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(Json.createObjectBuilder()
+            .add("error", e.getMessage())
+            .build().toString());
+         }
+
+         return ResponseEntity.status(HttpStatus.OK)
+         .contentType(MediaType.APPLICATION_JSON)
+         .body(Json.createObjectBuilder()
+         .add("message", )
+         .build().toString());
+         
+
+    }
+
+     @DeleteMapping(path="/tutorials/:id")
+    public ResponseEntity<String> deleteTutorial(@PathVariable Long id){
+
+       
+      
+        try {
+
+        }catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(500).body(
+                Json.createObjectBuilder()
+                .add("error", "Failure in deleting user")
+                .build().toString()
+            );
+        }
+
+        return ResponseEntity.ok().body(Json.createObjectBuilder()
+        .add("message", "Success in deleting tutorial")
+        .build().toString());
+    }
+
+
 }
